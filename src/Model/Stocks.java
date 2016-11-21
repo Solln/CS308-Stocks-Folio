09a093990b5bafc8e4983b6c58397c06cbd5d102
Darren Lowe <yxb15183@strath.ac.uk>
2016-11-21 15:00:47 +0000

    Started the Model

---
+++ src/Model/Stocks.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Darren on 21/11/2016.
+ */
+public class Stocks {
+
+    private ArrayList symbols;
+
+    public ArrayList getStocks(ArrayList symbols) {
+
+        this.symbols = symbols;
+
+
+
+
+        return null;
+    }
+
+
+
+
+}
