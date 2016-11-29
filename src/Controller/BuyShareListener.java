51b714f73b1d1462c64aa80a9d57192b25ec0aa2
Darren Lowe <yxb15183@strath.ac.uk>
2016-11-29 19:25:10 +0000

    Added 1st Listener

--- src/Controller/BuyShareListener.java
+++ src/Controller/BuyShareListener.java
@@ -1,7 +1,14 @@
 package Controller;
 
+import Model.Exceptions.MethodException;
+import Model.Exceptions.NoSuchTickerException;
+import Model.Exceptions.WebsiteDataException;
+import Model.FolioWorker;
+import Model.ManageFolios;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by Cairns on 28/11/2016.
@@ -11,6 +18,28 @@ public class BuyShareListener implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
 
+        int amountOfShares = 0;
+        String ticker = null;
+        int folioNum = 0;
+
+        ManageFolios worker = new FolioWorker();
+
+        ArrayList newStock = new ArrayList();
+
+        newStock.add(ticker);newStock.add("Douno");newStock.add(amountOfShares);
+        newStock.add("100");newStock.add(folioNum);
+
+        try {
+            worker.addStock(folioNum, newStock);
+        }
+
+        catch (NoSuchTickerException e1) {
+            e1.printStackTrace();
+        } catch (WebsiteDataException e1) {
+            e1.printStackTrace();
+        } catch (MethodException e1) {
+            e1.printStackTrace();
+        }
 
     }
 }
